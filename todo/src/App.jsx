47c81ab4a5c854bbf426269c8ecf1689e1210b9f
Jeremiah Samuel <livinalt@gmail.com>
2024-04-04 02:47:26 +0100

    fix

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -16,7 +16,7 @@ function App(props) {
     
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
+      <h1>My Todo</h1>
       <form>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
